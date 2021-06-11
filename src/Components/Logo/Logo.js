ec7dcfeffd5fd04001210e8e9ff055752650ca17
Agnibha Pal <pal.agnibha97@gmail.com>
2021-06-11 23:17:14 +0530

    tweaking tilt options

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -6,7 +6,7 @@ import Tilt from 'react-tilt'
 
 const Logo = () => {
     return(
-        <Tilt className="Tilt br3 shadow-2 ml5" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
+        <Tilt className="Tilt br3 shadow-2 ml5" options={{ max : 45, scale : 1.1 }} style={{ height: 100, width: 100 }} >
             <div className="Tilt-inner pa3">
                 <img src={brain} alt='logo'/>
             </div>
