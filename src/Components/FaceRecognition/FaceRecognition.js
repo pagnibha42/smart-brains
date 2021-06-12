591fe48d8f5270f4efe025b025320f51ba6bec90
Agnibha Pal <pal.agnibha97@gmail.com>
2021-06-12 20:39:49 +0530

    added br2 to the image

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -5,7 +5,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt3'>
-                <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto'/>
+                <img id='inputimage' style={{borderRadius: '0.5rem'}} src={imageUrl} alt='' width='500px' height='auto'/>
                 <div className='bounding-box' style={{borderRadius: '2rem', top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>            
             </div>
         </div>
